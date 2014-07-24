cf5a8d20b06f49a1e485db9377b9cc6e9f612bb9
Schmoller <steven.schmoll@gmail.com>
2014-07-24 12:25:34 +1000

    Added enchant signs

---
+++ src/main/java/au/com/addstar/ShopSigns/enchant/EnchantSign.java
@@ -0,0 +1,99 @@
+package au.com.addstar.ShopSigns.enchant;
+
+import net.milkbowl.vault.economy.Economy;
+
+import org.bukkit.ChatColor;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import au.com.addstar.ShopSigns.InteractiveSign;
+import au.com.addstar.ShopSigns.ShopSignsPlugin;
+
+public class EnchantSign extends InteractiveSign
+{
+	private Enchantment mEnchant;
+	private double mPrice;
+	private int mMaxLevel;
+	
+	public EnchantSign(Enchantment enchant, double price, int level)
+	{
+		mEnchant = enchant;
+		mPrice = price;
+		mMaxLevel = level;
+	}
+	
+	@Override
+	public void onLeftClick( Player player, ItemStack item )
+	{
+	}
+
+	@Override
+	public void onRightClick( Player player, ItemStack item )
+	{
+		if(!player.hasPermission("shopsigns.enchant.use"))
+		{
+			player.sendMessage(ChatColor.RED + "You do not have permission to enchant items");
+			return;
+		}
+		
+		if(item == null)
+		{
+			player.sendMessage(ChatColor.GOLD + "Please hold the item you wish to enchant in your hand, then right click this sign again to enchant.");
+			return;
+		}
+		
+		if(!mEnchant.canEnchantItem(item))
+		{
+			player.sendMessage(ChatColor.GOLD + "This item cannot accept this enchantment");
+			return;
+		}
+		
+		// Check for conflicts
+		for(Enchantment enchant : item.getEnchantments().keySet())
+		{
+			if(mEnchant.conflictsWith(enchant))
+			{
+				player.sendMessage(ChatColor.GOLD + "This item cannot accept this enchantment. It will conflict with another enchantment.");
+				return;
+			}
+		}
+		
+		Economy econ = ShopSignsPlugin.getEconomy();
+		
+		int current = item.getEnchantmentLevel(mEnchant);
+		double cost = (current + 1) * mPrice;
+		
+		if(player.hasPermission("shopsigns.enchant.free"))
+			cost = 0;
+		
+		if(current >= mMaxLevel)
+		{
+			player.sendMessage(ChatColor.GOLD + "This item cannot be enchanted. The enchantment is at max level.");
+			return;
+		}
+		
+		// Print overview and exit
+		if(!player.isSneaking())
+		{
+			player.sendMessage(ChatColor.GOLD + "The cost of this enchantment is " + econ.format(cost));
+			if(current != 0)
+				player.sendMessage(ChatColor.GOLD + "Your item will go from level " + current + " to level " + (current + 1));
+			
+			player.sendMessage(ChatColor.GOLD + "Shift + Right click this sign to purchase the enchantment");
+			return;
+		}
+		
+		if(!econ.has(player, cost))
+		{
+			player.sendMessage(ChatColor.RED + "You have insufficient funds to purchase this enchantment");
+			return;
+		}
+		
+		econ.withdrawPlayer(player, cost);
+		
+		item.addUnsafeEnchantment(mEnchant, current+1);
+		player.sendMessage(ChatColor.GREEN + "Successfully enchanted your item");
+	}
+
+}
---
+++ src/main/java/au/com/addstar/ShopSigns/enchant/EnchantSignDefinition.java
@@ -0,0 +1,120 @@
+package au.com.addstar.ShopSigns.enchant;
+
+import java.util.HashMap;
+
+import org.bukkit.ChatColor;
+import org.bukkit.block.Sign;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.event.block.SignChangeEvent;
+
+import au.com.addstar.ShopSigns.SignDefinition;
+import au.com.addstar.ShopSigns.Util;
+
+public class EnchantSignDefinition extends SignDefinition
+{
+	private HashMap<String, Enchantment> mEnchantments = new HashMap<String, Enchantment>();
+	
+	public EnchantSignDefinition()
+	{
+		mEnchantments.put("projectile", Enchantment.PROTECTION_PROJECTILE);
+		mEnchantments.put("fireprotection", Enchantment.PROTECTION_FIRE);
+		mEnchantments.put("aquaaffinity", Enchantment.WATER_WORKER);
+		mEnchantments.put("protection", Enchantment.PROTECTION_ENVIRONMENTAL);
+		mEnchantments.put("sharpness", Enchantment.DAMAGE_ALL);
+		mEnchantments.put("falling", Enchantment.PROTECTION_FALL);
+		mEnchantments.put("thorns", Enchantment.THORNS);
+		mEnchantments.put("fortune", Enchantment.LOOT_BONUS_BLOCKS);
+		mEnchantments.put("fireaspect", Enchantment.FIRE_ASPECT);
+		mEnchantments.put("blast", Enchantment.PROTECTION_EXPLOSIONS);
+		mEnchantments.put("damage", Enchantment.ARROW_DAMAGE);
+		mEnchantments.put("power", Enchantment.ARROW_DAMAGE);
+		mEnchantments.put("flame", Enchantment.ARROW_FIRE);
+		mEnchantments.put("punch", Enchantment.ARROW_KNOCKBACK);
+		mEnchantments.put("infinity", Enchantment.ARROW_INFINITE);
+		mEnchantments.put("looting", Enchantment.LOOT_BONUS_MOBS);
+		mEnchantments.put("respiration", Enchantment.OXYGEN);
+		mEnchantments.put("efficiency", Enchantment.DIG_SPEED);
+		mEnchantments.put("knockback", Enchantment.KNOCKBACK);
+		mEnchantments.put("silktouch", Enchantment.SILK_TOUCH);
+		mEnchantments.put("vsarthropods", Enchantment.DAMAGE_ARTHROPODS);
+		mEnchantments.put("arthropods", Enchantment.DAMAGE_ARTHROPODS);
+		mEnchantments.put("unbreaking", Enchantment.DURABILITY);
+		mEnchantments.put("vsundead", Enchantment.DAMAGE_UNDEAD);
+		mEnchantments.put("smite", Enchantment.DAMAGE_UNDEAD);
+		mEnchantments.put("lure", Enchantment.LURE);
+		mEnchantments.put("luckofsea", Enchantment.LUCK);
+	}
+	
+	@Override
+	public EnchantSign parse( Sign sign ) throws IllegalArgumentException
+	{
+		if(!sign.getLine(0).equals(ChatColor.GOLD + "[Enchant]"))
+			return null;
+		
+		Enchantment enchant = parseEnchant(sign.getLine(1));
+		return new EnchantSign(enchant, Util.parsePrice(sign.getLine(2), 3), parseMaxLevel(sign.getLine(3), enchant));
+	}
+
+	@Override
+	public boolean initialize( SignChangeEvent event ) throws IllegalArgumentException, IllegalAccessException
+	{
+		if(!ChatColor.stripColor(event.getLine(0)).equalsIgnoreCase("[enchant]"))
+			return false;
+		
+		if(!event.getPlayer().hasPermission("shopsigns.enchant.create"))
+			throw new IllegalAccessException();
+		
+		Enchantment ench = parseEnchant(event.getLine(1));
+		Util.parsePrice(event.getLine(2), 3);
+		int level = parseMaxLevel(event.getLine(3), ench);
+		
+		event.setLine(0, ChatColor.GOLD + "[Enchant]");
+		event.setLine(3, String.valueOf(level));
+		
+		return true;
+	}
+	
+	@SuppressWarnings( "deprecation" )
+	private Enchantment parseEnchant(String line) throws IllegalArgumentException
+	{
+		Enchantment enchant = mEnchantments.get(line.toLowerCase());
+		if(enchant == null)
+			enchant = Enchantment.getByName(line.toUpperCase());
+		if(enchant == null)
+		{
+			try
+			{
+				int id = Integer.parseInt(line);
+				enchant = Enchantment.getById(id);
+			}
+			catch(NumberFormatException e)
+			{
+			}
+		}
+		
+		if(enchant == null)
+			throw new IllegalArgumentException("Line 2: Unknown enchantment " + line);
+		
+		return enchant;
+	}
+	
+	private int parseMaxLevel(String line, Enchantment enchant) throws IllegalArgumentException
+	{
+		if(line.isEmpty())
+			return enchant.getMaxLevel();
+		
+		try
+		{
+			int level = Integer.parseInt(line);
+			if(level <= 0 || level > 10)
+				throw new IllegalArgumentException("Line 4: Maximum level must be between 1 and 10");
+			
+			return level;
+		}
+		catch(NumberFormatException e)
+		{
+			throw new IllegalArgumentException("Line 4: Maximum level must be between 1 and 10, or left blank to use highest level for the enchant");
+		}
+	}
+
+}
